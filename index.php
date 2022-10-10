81cc99a48aace2b3f6bf3cc592b00127631177b1
eetosuu <77057144+eetosuu@users.noreply.github.com>
2022-10-11 02:45:41 +0300

    less3

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
-while (true) {
-    $res = readline("В каком году произошло крещение Руси?\n");
-    switch ($res) {
-        case 810:
-            echo "Неверно\n";
-            break 2;
-        case 990:
-            echo "Верно\n";
-            break 2;
-        case 740:
-            echo "Неверно\n";
-            break 2;
-    }
+$firstMultiplier = [2, 4, 6, 8, 11, 3, 7, 12, 34, 124];
+$secondMultiplier = [666, 37, 18, 94, 44, 71, 52, 51, 91, 26];
+
+for ($index = 0; $index < count($firstMultiplier); $index++) {
+    var_dump($firstMultiplier[$index] * $secondMultiplier[$index]);
+};
+
+$name = readline("Как вас зовут?\n");
+$wishes = ['счастья', 'здоровья', 'воображения', 'успеха'];
+$epithet = ['космического', 'бесконечного', 'безудержного', 'отличного'];
+$congratulation = [];
+for ($i = 1; $i <= 3; $i++) {
+    $wishesRandom = array_rand($wishes);
+    $epithetRandom = array_rand($epithet);
+    $value = $epithet[$epithetRandom] . ' ' . $wishes[$wishesRandom];
+    $congratulation[] = $value;
+    unset($wishes[$wishesRandom]);
+    unset($epithet[$epithetRandom]);
 }
+$stringCongratulation = implode(', ', $congratulation);
+
+echo 'Дорогой ' . $name . ', от всего сердца поздравляю тебя с днем рождения, желаю ' . $stringCongratulation . '!';
